b7cc41db1e1f48514f2e630a910e98b111afaea4
lil-12 <90734020+lil-12@users.noreply.github.com>
2021-09-22 12:20:35 -0400

    Update AdminAccountTest.java

--- AdminAccountTest.java
+++ AdminAccountTest.java
@@ -27,14 +27,14 @@ class AdminAccountTest {
 	//tests the getUsername method
 	@Test
 	void testGetUsername() {
-		assert(admin.getUsername().equals("admin1"));
+		assert(admin.getUsername().equals("admin1"):"GetUserName failed");
 	}
 	
 	//tests the verifyPassword method
 	@Test
 	void testVerifyPassword()
 	{
-		assert(admin.verifyPassword("12345"));
+		assert(admin.verifyPassword("12345"): "VerifyPassword Failed");
 	}
 	
 	//tests the setPassword method
@@ -42,14 +42,14 @@ class AdminAccountTest {
 	void testSetPassword()
 	{
 		admin.setPassword("67890");
-		assert(admin.verifyPassword("67890"));
+		assert(admin.verifyPassword("67890"): "SetPassword Failed");
 	}
 	
 	//tests the toString method
 	@Test
 	void testToString()
 	{
-		assert(admin.toString().equals("\nUsername: " + "admin1"+ "\nClass Type: " + admin.getClass()));
+		assert(admin.toString().equals("\nUsername: " + "admin1"+ "\nClass Type: " + admin.getClass()):"ToString failed");
 	}
 
 }
